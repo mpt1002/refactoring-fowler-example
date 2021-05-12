104fdc7d678d627b36aeb19f928774b2cf1f8395
mpt1002 <57134968+mpt1002@users.noreply.github.com>
2021-05-12 08:13:04 +0200

    Move Method

--- src/ubu/gii/dass/refactoring/Customer.java
+++ src/ubu/gii/dass/refactoring/Customer.java
@@ -42,7 +42,7 @@ public class Customer {
 			thisAmount = each.getCharge();
 			
 			// add frequent renter points
-			frequentRenterPoints += getFrecuentRenterPoints(each);
+			frequentRenterPoints += each.getFrecuentRenterPoints();
 			// show figures for this rental
 			result += "\t" + each.getMovie().getTitle() + "\t"
 					+ String.valueOf(thisAmount) + "\n";
@@ -53,14 +53,5 @@ public class Customer {
 		result += "You earned " + String.valueOf(frequentRenterPoints)
 				+ " frequent renter points";
 		return result;
-	}
-
-	private int getFrecuentRenterPoints(Rental each) {
-		int frequentRenterPoints = 1;
-		// add bonus for a two day new release rental
-		if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE)
-				&& each.getDaysRented() > 1)
-			frequentRenterPoints++;
-		return frequentRenterPoints;
 	}
 }
